586261643a805636b961af4a839b4e5ff13b10fc
Manuel Venegas <mengasis@outlook.com>
2018-12-30 19:35:42 -0300

    Added global styles with styled-components

---
+++ client/src/components/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Comic Sans MS",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}`
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './containers/App'
+import GlobalStyles from './components/GlobalStyles'
+
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyles />
+  </>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
