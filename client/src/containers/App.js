d037fbc34df64ce6a6eb54165ce5c83a5e347a56
Manuel Venegas <mengasis@outlook.com>
2018-12-31 19:47:52 -0300

    Connected App component to api

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 
+import { cities, subscribeToWeather } from '../api'
+
 class App extends Component {
+  state = { weather: {} }
+
+  componentDidMount() {
+    subscribeToWeather((err, data) => this.setState({ weather: data }))
+  }
+
   render() {
-    return <div />
+    return (
+      <div>
+        <span>Weather</span>
+        {cities.map(city => (
+          <p key={city.id}>
+            {city.name}: {this.state.weather[city.id]}
+          </p>
+        ))}
+      </div>
+    )
   }
 }
 
