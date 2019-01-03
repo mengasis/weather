a3939da14ee0541229469b7222c59741d2ba6b9a
Manuel Venegas <mengasis@outlook.com>
2019-01-03 01:48:37 -0300

    Created method to asynchronously map cities

--- server/services/weather.js
+++ server/services/weather.js
@@ -1,4 +1,6 @@
 const { getCurrentData } = require('./darkSkyApi')
+const cities = require('../config/cities')
+const retry = require('../utils/retry')
 
 async function getWeather(city = {}) {
   try {
@@ -13,4 +15,17 @@ async function getWeather(city = {}) {
   }
 }
 
-module.exports = { getWeather }
+function getAllWeather() {
+  return Promise.all(
+    cities.map(async city =>
+      retry(
+        () => getWeather(city),
+        err => {
+          console.error('Error: ' + err)
+        }
+      )
+    )
+  )
+}
+
+module.exports = { getWeather, getAllWeather }
