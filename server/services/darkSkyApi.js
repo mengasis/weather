0aa38d962d185a712cfd362304260535ef79cd14
Manuel Venegas <mengasis@outlook.com>
2019-01-03 01:37:19 -0300

    Filtered only the current info of the api

--- server/services/darkSkyApi.js
+++ server/services/darkSkyApi.js
@@ -1,8 +1,11 @@
 const https = require('https')
 const config = require('../config/api')
 
-function getData(latitude, longitude) {
-  const url = `${config.API_HOST}/${config.API_KEY}s/${latitude},${longitude}`
+function getCurrentData(latitude = '', longitude = '') {
+  const exclude = ['minutely', 'hourly', 'daily', 'flags'].join(',')
+  const url = `${config.API_HOST}/${
+    config.API_KEY
+  }/${latitude},${longitude}?exclude=${exclude}`
 
   return new Promise((resolve, reject) => {
     https.get(url, res => {
