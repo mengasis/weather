3184bbbced26424bca31ec58865d7df951e74a45
Manuel Venegas <mengasis@outlook.com>
2019-01-03 23:38:27 -0300

    Added error types

--- server/utils/customError.js
+++ server/utils/customError.js
@@ -5,3 +5,23 @@ function createError(name = 'Error', message = '') {
   Error.captureStackTrace(err, createError)
   return err
 }
+
+const UNFORTUNATE = createError('UNFORTUNATE', 'How unfortunate! The API Request Failed.')
+
+const NOT_FOUND = createError(
+  'NOT FOUND',
+  `The origin server did not find a current representation for the target 
+  resource or is not willing to disclose that one exists.`
+)
+
+const FORBIDDEN = createError(
+  'FORBIDDEN',
+  'The server understood the request but refuses to authorize it.'
+)
+
+module.exports = {
+  createError,
+  UNFORTUNATE,
+  FORBIDDEN,
+  NOT_FOUND
+}
