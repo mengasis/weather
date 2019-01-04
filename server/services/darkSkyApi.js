8df090b0c27fdebb0b6a14c3e82994277e9f6c19
Manuel Venegas <mengasis@outlook.com>
2019-01-03 23:39:28 -0300

    Refactor api with axios

--- server/services/darkSkyApi.js
+++ server/services/darkSkyApi.js
@@ -1,33 +1,27 @@
-const https = require('https')
-const config = require('../config/api')
+const axios = require('axios')
+const { API_HOST, API_KEY } = require('../config/api')
+const customError = require('../utils/customError')
 
 function getCurrentData(latitude = '', longitude = '') {
-  const exclude = ['minutely', 'hourly', 'daily', 'flags'].join(',')
-  const url = `${config.API_HOST}/${
-    config.API_KEY
-  }/${latitude},${longitude}?exclude=${exclude}`
+  const url = `${API_HOST}/${API_KEY}/${latitude},${longitude}`
+  const opts = { url, params: { exclude: ['minutely', 'hourly', 'daily', 'flags'] } }
 
-  return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      let data = []
+  return axios(url, opts)
+    .then(({ data }) => data)
+    .catch(err => {
+      const { response = { status: 0 } } = err
 
-      const { statusCode } = res
+      switch (response.status) {
+        case 404:
+          throw customError.NOT_FOUND
 
-      if (statusCode >= 400 && statusCode <= 500)
-        reject(new Error(statusCode), {
-          latitude,
-          longitude
-        })
+        case 403:
+          throw customError.FORBIDDEN
 
-      res.on('data', d => {
-        data.push(d)
-      })
-
-      res.on('end', () => resolve(JSON.parse(Buffer.concat(data).toString())))
-
-      res.on('error', reject)
+        default:
+          throw customError.createError(err)
+      }
     })
-  })
 }
 
 module.exports = { getCurrentData }
